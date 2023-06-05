c49ba251408959cc91f1a35d5096c728c5243c02
AlejandroRivera2306 <ale_jandro2306@hotmail.com>
2023-06-05 15:53:17 -0500

    version 1.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,7 @@
-// import React, { useState, useEffect} from 'react'
-// import Header from './components/Header'
-// import Formularios from './components/Formulario.jsx'
-// import ListadoPacientes from './components/ListadoPacientes.jsx'
-
-// function App() {
-//   const [pacientes, setPacientes] = useState([]);
-//   const [paciente, setPaciente] = useState({});
-
-//   useEffect(() => {
-//     const obtenerLS = () => {
-//       const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []; 
-//       console.log(pacientesLS)
-//       setPacientes(pacientesLS);
-//     };
-//     obtenerLS();
-//   }, []);
-  
-
-//   useEffect(() => {
-//     localStorage.setItem('pacientes', JSON.stringify(pacientes));
-//   }, [pacientes]);
-  
-  
-
-//   const eliminarPaciente = (id) =>{
-//    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
-//    setPacientes(pacientesActualizados)
-//   }
-
-//   return (
-//     <div className='container mx-auto mt-20'>
-//       <Header/>
-//       <div className='mt-12 md:flex'> 
-//       <Formularios
-//         pacientes={pacientes}
-//         setPacientes={setPacientes}
-//         paciente={paciente}
-//         setPaciente={setPaciente}
-//       />
-//       <ListadoPacientes
-//       pacientes={pacientes}
-//       setPaciente={setPaciente}
-//       eliminarPaciente={eliminarPaciente}
-//       />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
-import Formularios from './components/Formulario.jsx';
-import ListadoPacientes from './components/ListadoPacientes.jsx';
+import React, { useState, useEffect} from 'react'
+import Header from './components/Header'
+import Formularios from './components/Formulario.jsx'
+import ListadoPacientes from './components/ListadoPacientes.jsx'
 
 function App() {
   const [pacientes, setPacientes] = useState([]);
@@ -61,39 +9,89 @@ function App() {
 
   useEffect(() => {
     const obtenerLS = () => {
-      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []
-      setPacientes(pacientesLS)
-    }
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? []; 
+      console.log(pacientesLS)
+      setPacientes(pacientesLS);
+    };
     obtenerLS();
   }, []);
+  
 
   useEffect(() => {
-    localStorage.setItem('pacientes', JSON.stringify( pacientes ));
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
   }, [pacientes]);
+  
+  
 
-  const eliminarPaciente = (id) => {
-    const pacientesActualizados = pacientes.filter((paciente) => paciente.id !== id);
-    setPacientes(pacientesActualizados);
-  };
+  const eliminarPaciente = (id) =>{
+   const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+   setPacientes(pacientesActualizados)
+  }
 
   return (
     <div className='container mx-auto mt-20'>
-      <Header />
-      <div className='mt-12 md:flex'>
-        <Formularios
-          pacientes={pacientes}
-          setPacientes={setPacientes}
-          paciente={paciente}
-          setPaciente={setPaciente}
-        />
-        <ListadoPacientes
-          pacientes={pacientes}
-          setPaciente={setPaciente}
-          eliminarPaciente={eliminarPaciente}
-        />
+      <Header/>
+      <div className='mt-12 md:flex'> 
+      <Formularios
+        pacientes={pacientes}
+        setPacientes={setPacientes}
+        paciente={paciente}
+        setPaciente={setPaciente}
+      />
+      <ListadoPacientes
+      pacientes={pacientes}
+      setPaciente={setPaciente}
+      eliminarPaciente={eliminarPaciente}
+      />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// import React, { useState, useEffect } from 'react';
+// import Header from './components/Header';
+// import Formularios from './components/Formulario.jsx';
+// import ListadoPacientes from './components/ListadoPacientes.jsx';
+
+// function App() {
+//   const [pacientes, setPacientes] = useState([]);
+//   const [paciente, setPaciente] = useState({});
+  
+//   useEffect(() => {
+//     const obtenerLS = () => {
+//       const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+//       setPacientes(pacientesLS);
+//       console.log(pacientesLS);
+//       localStorage.setItem('pacientes', JSON.stringify(pacientesLS));
+//     };
+//     obtenerLS();
+//   }, []);
+
+//   const eliminarPaciente = (id) => {
+//     const pacientesActualizados = pacientes.filter((paciente) => paciente.id !== id);
+//     setPacientes(pacientesActualizados);
+//   };
+ 
+//   return (
+//     <div className='container mx-auto mt-20'>
+//       <Header />
+//       <div className='mt-12 md:flex'>
+//         <Formularios
+//           pacientes={pacientes}
+//           setPacientes={setPacientes}
+//           paciente={paciente}
+//           setPaciente={setPaciente}
+//         />
+//         <ListadoPacientes
+//           pacientes={pacientes}
+//           setPaciente={setPaciente}
+//           eliminarPaciente={eliminarPaciente}
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
